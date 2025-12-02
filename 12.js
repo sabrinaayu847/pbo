c582893f7bf715bc6d99d6db0c6a0c420f45db60
sabrinaayu847 <sabrinaayukartika@gmail.com>
2025-12-03 01:57:45 +0700

    Update 12.js

--- 12.js
+++ 12.js
@@ -14,5 +14,26 @@ const daftarPendaftar = JSON.parse(dataString);
 // Tampilkan data
 console.log("Daftar Pendaftar:");
 daftarPendaftar.forEach(function(p) {
-    console.log(${p.nama} - ${p.email} - ${p.gender});
+    console.log(`${p.nama} - ${p.email} - ${p.gender}`);
+});
+
+
+// Data mahasiswa
+const mahasiswa = [
+    { nama: "Andi", umur: 21, jurusan: "Informatika" },
+    { nama: "Budi", umur: 22, jurusan: "Sistem Informasi" }
+];
+
+// Simpan ke localStorage
+localStorage.setItem("mahasiswa", JSON.stringify(mahasiswa));
+console.log("Data mahasiswa disimpan di localStorage!");
+
+// Ambil kembali dari localStorage
+const jsonString = localStorage.getItem("mahasiswa");
+const mahasiswaList = JSON.parse(jsonString);
+
+// Tampilkan daftar mahasiswa
+console.log("Daftar Mahasiswa:");
+mahasiswaList.forEach(mhs => {
+    console.log(`${mhs.nama} - ${mhs.jurusan}`);
 });
